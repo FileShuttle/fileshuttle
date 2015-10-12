0cfa86ef37a7ad3914cd6968acaef8e0768efa50
Anthony Somerset <anthony@somersettechsolutions.co.uk>
2015-10-12 22:27:54 +0200

    code sign scripts to try and get code signed builds working

--- Scripts/codesign-frameworks2.sh
+++ Scripts/codesign-frameworks2.sh
@@ -5,3 +5,9 @@
 #
 #  Created by Anthony Somerset on 12/10/2015.
 #
+LOCATION="${BUILT_PRODUCTS_DIR}"/"${FRAMEWORKS_FOLDER_PATH}"
+IDENTITY="Mac Developer: Anthony Somerset (A4ZG8D27HY)"
+codesign --verbose --force --sign "$IDENTITY" "$LOCATION/CURLHandle.framework/Versions/A"
+codesign --verbose --force --sign "$IDENTITY" "$LOCATION/Growl.framework/Versions/A"
+codesign --verbose --force --sign "$IDENTITY" "$LOCATION/ShortcutRecorder.framework/Versions/A"
+codesign --verbose --force --sign "$IDENTITY" "$LOCATION/RegexKit.framework/Versions/A"
